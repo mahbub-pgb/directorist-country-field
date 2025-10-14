f08dc30a3b2a6306dddcb199c325dee0ff2090f5
mahbub-pgb <mahbub.progressivebyte@gmail.com>
2025-10-14 13:28:06 +0600

    update

--- includes/Common.php
+++ includes/Common.php
@@ -13,7 +13,7 @@ class Common {
         // add_action( 'atbdp_listing_updated', [ $this, 'save_countries' ] );
 
         add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_assets_admin' ] );
-        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets_admin' ] );
+        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets_front' ] );
     }
 
    
@@ -32,10 +32,10 @@ class Common {
      * Enqueue CSS/JS for admin
      */
     public function enqueue_assets_admin() {
-        // wp_enqueue_style( 'select2', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css', [], '4.1.0' );
+        wp_enqueue_style( 'select2', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css', [], '4.1.0' );
     
-        // // JS
-        // wp_enqueue_script( 'select2', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', ['jquery'], '4.1.0', true );
+        // JS
+        wp_enqueue_script( 'select2', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', ['jquery'], '4.1.0', true );
         wp_enqueue_style(
             'dlf-common',
             DLF_PLUGIN_URL . 'assets/css/common.css',
@@ -51,24 +51,24 @@ class Common {
         );
     }
 
-    // /**
-    //  * Enqueue CSS/JS for frontend
-    //  */
-    // public function enqueue_assets_front() {
-    //     wp_enqueue_style(
-    //         'dlf-frontend',
-    //         DLF_PLUGIN_URL . 'assets/css/frontend.css',
-    //         [],
-    //         '1.0.0'
-    //     );
-    //     wp_enqueue_script(
-    //         'dlf-frontend',
-    //         DLF_PLUGIN_URL . 'assets/js/frontend.js',
-    //         [ 'jquery' ],
-    //         '1.0.0',
-    //         true
-    //     );
-    // }
+    /**
+     * Enqueue CSS/JS for frontend
+     */
+    public function enqueue_assets_front() {
+        wp_enqueue_style(
+            'dlf-frontend',
+            DLF_PLUGIN_URL . 'assets/css/frontend.css',
+            [],
+            '1.0.0'
+        );
+        wp_enqueue_script(
+            'dlf-frontend',
+            DLF_PLUGIN_URL . 'assets/js/frontend.js',
+            [ 'jquery' ],
+            '1.0.0',
+            true
+        );
+    }
 
    
 }
