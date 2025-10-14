422a93c367460be2be3177b0e112a1bf6bbfb466
mahbub-pgb <mahbub.progressivebyte@gmail.com>
2025-10-14 16:17:49 +0600

    update

--- includes/Front.php
+++ includes/Front.php
@@ -7,6 +7,19 @@ class Front {
 
     public function __construct() {
         add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets' ] );
+        add_action( 'wp_head', [ $this, 'head' ] );
+    }
+
+    public function head(){
+        $all_countries = get_terms([
+            'taxonomy'   => 'country_expert',
+            'hide_empty' => false,
+        ]);
+
+        
+
+
+        pri( $all_countries );
     }
     public function enqueue_assets() {
         // Add frontend JS or CSS if needed later
