75161163bbb45bef05dfae4cd04e52f736a957d9
mahbub-pgb <mahbub.progressivebyte@gmail.com>
2025-10-14 13:03:28 +0600

    css and js file added

--- includes/Common.php
+++ includes/Common.php
@@ -7,47 +7,16 @@ class Common {
 
     public function __construct() {
         // Register custom taxonomy
-        add_action( 'init', [ $this, 'register_country_taxonomy' ], 11 );
 
         // Save taxonomy terms when listing is inserted or updated
-        add_action( 'atbdp_listing_inserted', [ $this, 'save_countries' ] );
-        add_action( 'atbdp_listing_updated', [ $this, 'save_countries' ] );
+        // add_action( 'atbdp_listing_inserted', [ $this, 'save_countries' ] );
+        // add_action( 'atbdp_listing_updated', [ $this, 'save_countries' ] );
 
-        // Reset taxonomy terms when empty
-        add_action( 'atbdp_before_listing_update', [ $this, 'reset_countries' ] );
-
-        // Hook into Directorist field registration
-        add_action( 'plugins_loaded', [ $this, 'register_country_field' ] );
+        add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_assets_admin' ] );
+        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets_front' ] );
     }
 
-    /**
-     * Register the "Country Expert" taxonomy
-     */
-    public function register_country_taxonomy() {
-        register_taxonomy(
-            'country_expert',
-            'at_biz_dir',
-            [
-                'labels' => [
-                    'name'              => __( 'Countries Expert', 'directorist-country-field' ),
-                    'singular_name'     => __( 'Country Expert', 'directorist-country-field' ),
-                    'search_items'      => __( 'Search Countries Expert', 'directorist-country-field' ),
-                    'all_items'         => __( 'All Countries Expert', 'directorist-country-field' ),
-                    'edit_item'         => __( 'Edit Country Expert', 'directorist-country-field' ),
-                    'update_item'       => __( 'Update Country Expert', 'directorist-country-field' ),
-                    'add_new_item'      => __( 'Add New Country Expert', 'directorist-country-field' ),
-                    'new_item_name'     => __( 'New Country Expert Name', 'directorist-country-field' ),
-                    'menu_name'         => __( 'Countries Expert', 'directorist-country-field' ),
-                ],
-                'hierarchical'      => false,
-                'show_ui'           => true,
-                'show_admin_column' => true,
-                'show_in_rest'      => true,
-                'public'            => true,
-                'rewrite'           => [ 'slug' => 'country-expert' ],
-            ]
-        );
-    }
+   
 
     /**
      * Save taxonomy terms when listing is inserted or updated
@@ -59,47 +28,43 @@ class Common {
         }
     }
 
-    /**
-     * Reset taxonomy terms if none selected
+     /**
+     * Enqueue CSS/JS for admin
      */
-    public function reset_countries( $listing_id ) {
-        if ( empty( $_POST['country_expert'] ) ) {
-            wp_set_object_terms( $listing_id, [], 'country_expert', false );
-        }
+    public function enqueue_assets_admin() {
+        wp_enqueue_style(
+            'dlf-admin',
+            DLF_PLUGIN_URL . 'assets/css/admin.css',
+            [],
+            '1.0.0'
+        );
+        wp_enqueue_script(
+            'dlf-admin',
+            DLF_PLUGIN_URL . 'assets/js/admin.js',
+            [ 'jquery' ],
+            '1.0.0',
+            true
+        );
     }
 
     /**
-     * Register the custom field type with Directorist
+     * Enqueue CSS/JS for frontend
      */
-    public function register_country_field() {
-        if ( ! class_exists( 'Directorist_Fields' ) ) return;
-
-        // Include the custom field class
-        require_once DLF_PLUGIN_DIR . 'includes/class-directorist-country-field.php';
-
-        // Map the field key to our field class
-        add_filter( 'directorist_field_types', function ( $fields ) {
-            $fields['country_expert'] = 'Directorist_Country_Field';
-            return $fields;
-        });
-
-        // Register the field in the form builder
-        add_filter( 'directorist_form_field_options', function ( $fields ) {
-            $fields['country_expert'] = [
-                'type'        => 'country_expert',
-                'label'       => __( 'Country Expert', 'directorist' ),
-                'taxonomy'    => 'country_expert',
-                'field_class' => 'Directorist_Country_Field',
-            ];
-            return $fields;
-        });
-
-        // Load the custom template for this field
-        add_filter( 'directorist_field_template_path', function ( $template, $field_type ) {
-            if ( 'country_expert' === $field_type ) {
-                return DLF_PLUGIN_DIR . 'includes/template/country.php';
-            }
-            return $template;
-        }, 10, 2 );
+    public function enqueue_assets_front() {
+        wp_enqueue_style(
+            'dlf-frontend',
+            DLF_PLUGIN_URL . 'assets/css/frontend.css',
+            [],
+            '1.0.0'
+        );
+        wp_enqueue_script(
+            'dlf-frontend',
+            DLF_PLUGIN_URL . 'assets/js/frontend.js',
+            [ 'jquery' ],
+            '1.0.0',
+            true
+        );
     }
+
+   
 }
--- includes/Front.php
+++ includes/Front.php
@@ -8,10 +8,6 @@ class Front {
     public function __construct() {
         add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_assets' ] );
     }
-
-    
-
-
     public function enqueue_assets() {
         // Add frontend JS or CSS if needed later
     }
